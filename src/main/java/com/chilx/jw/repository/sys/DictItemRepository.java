24e81c7e45346474368557e58879c5c0c94515d0
chiliangxiao@golaxy.cn <chiliangxiao>
2021-10-12 14:16:01 +0800

    字典数据

---
+++ src/main/java/com/chilx/jw/repository/sys/DictItemRepository.java
@@ -0,0 +1,11 @@
+package com.chilx.jw.repository.sys;
+
+import com.chilx.jw.common.base.BaseRepository;
+import com.chilx.jw.entity.sys.SysDictTypeEntity;
+
+/**
+ * @author chilx
+ * @date 2021/10/11
+ **/
+public interface DictItemRepository extends BaseRepository<SysDictTypeEntity, Integer> {
+}
